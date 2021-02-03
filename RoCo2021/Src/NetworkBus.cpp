4e9f296c0d07dfe613192e196432e8572a2397d6
homerdon <homerdon0@gmail.com>
2021-02-03 17:56:04 +0100

    reorganized RoCo to facilitate unit-testing, added a Google Test Suite in MainNetworkTest.cpp that tests the functionality of RoCo with the packets in Protocol21W3 (also added to replace the outdated protocol 20W18), all the tests added in this commit passed successfully

---
+++ RoCo2021/Src/NetworkBus.cpp
@@ -0,0 +1,34 @@
+/*
+ * NetworkBus.cpp
+ *
+ *  Created on: 8 May 2020
+ *      Author: Arion
+ */
+
+#include "Build/Build.h"
+
+
+#ifdef BUILD_WITH_NETWORK_BUS
+
+
+#include "NetworkBus.h"
+#include "Protocol/Protocol.h"
+
+NetworkBus::NetworkBus(IODriver* driver) : IOBus(driver, network_frame, sizeof(network_frame)) {
+	define<PingPacket>(0);
+	define<RequestPacket>(1);
+	define<ResponsePacket>(2);
+	define<ProgressPacket>(3);
+	define<DataPacket>(4);
+	define<Avionics_BaroTempPacket>(5);
+	define<Avionics_AccelMagPacket>(6);
+	define<Handling_GripperPacket>(7);
+	define<Power_VoltagePacket>(8);
+    define<Power_CurrentPacket>(9);
+    define<Power_SystemPacket>(10);
+    define<Science_MeasurePacket>(11);
+	define<ErrorPacket>(63);
+}
+
+
+#endif /* BUILD_WITH_NETWORK_BUS */
---
+++ RoCo2021/Src/Protocol/Protocol.h
@@ -0,0 +1,23 @@
+/*
+ * Protocol.h
+ *
+ *  Created on: 12 Feb 2020
+ *      Author: Arion
+ */
+
+#ifndef PROTOCOL_PROTOCOL_H_
+#define PROTOCOL_PROTOCOL_H_
+
+#include "../Build/Build.h"
+
+
+//#ifdef PROTOCOL_20W18
+//#include "Protocol20W18.h"
+
+#ifdef PROTOCOL_21W3
+#include "Protocol21W3.h"
+
+#endif /* PROTOCOL_21W3 */
+
+
+#endif /* PROTOCOL_PROTOCOL_H_ */
