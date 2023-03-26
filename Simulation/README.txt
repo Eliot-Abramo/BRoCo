87fb67c1c105b0db9500b4bb368cc285b4e98ec9
YassineB <ybakkali8@gmail.com>
2023-03-26 19:46:57 +0200

    Ros2 implementation of a dummy publisher and subscriber.

--- Simulation/README.txt
+++
@@ -1,5 +0,0 @@
-This folder contains the files used to simulate the RoCo communication network between the Control Station and the different components of the rover. 
-
-There are two subnetworks,
-- one where the CS communicates to the Avionics (and vice-versa) via the Jetson (which is the server of the network) (5GHz link) and 
-- one where the CS communcicates to the Power Supply (and vice-versa) directly (2.4GHz link). The power supply is the server. 
---
+++ src/RocoRos/src/RocoRos.cpp
@@ -0,0 +1,108 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include <fcntl.h>
+#include <errno.h>
+#include <termios.h>
+#include <iostream>
+#include <unistd.h>
+#include <chrono>
+
+#include "../include/RocoRos/RoCo.h"
+#include "../include/RocoRos/UDevDriver.h"
+#include "../include/RocoRos/handlers_cs.h"
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/int32.hpp"
+
+using namespace std::chrono_literals;
+
+/* This example creates a subclass of Node and uses std::bind() to register a
+* member function as a callback from the timer. */
+
+
+// class MinimalPublisher : public rclcpp::Node
+// {
+//   public:
+//     MinimalPublisher()
+//     : Node("minimal_publisher"), count_(0)
+//     {
+//       publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+//       timer_ = this->create_wall_timer(
+//       500ms, std::bind(&MinimalPublisher::timer_callback, this));
+//     }
+
+//   private:
+//     void timer_callback()
+//     {
+//       auto message = std_msgs::msg::String();
+//       message.data = "Hello, world! " + std::to_string(count_++);
+//       RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+//       publisher_->publish(message);
+//     }
+//     rclcpp::TimerBase::SharedPtr timer_;
+//     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+//     size_t count_;
+// };
+
+
+
+int main(int argc, char * argv[])
+{
+  UDevDriver* driver = new UDevDriver("/dev/ttyUSB1");
+
+  struct termios tty;
+
+  if(tcgetattr(driver->getFD(), &tty) != 0) {
+      printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
+  }
+
+  tty.c_cflag &= ~PARENB;
+  tty.c_cflag &= ~CSTOPB;
+  tty.c_cflag &= ~CSIZE;
+  tty.c_cflag |= CS8;
+  tty.c_cflag &= ~CRTSCTS;
+  tty.c_cflag |= CREAD | CLOCAL;
+  tty.c_cflag &= ~HUPCL;
+
+  tty.c_lflag = 0;
+
+  tty.c_iflag &= ~(IXON | IXOFF | IXANY);
+  tty.c_iflag |= 0; 
+  tty.c_iflag &= ~(IGNPAR | IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL | INPCK | IMAXBEL);
+  tty.c_iflag &= ~IUTF8;
+
+  tty.c_oflag = 0;
+
+  tty.c_cc[VTIME] = 0;
+  tty.c_cc[VMIN] = 0;
+
+  cfsetispeed(&tty, B921600);
+  cfsetospeed(&tty, B921600);
+
+  if (tcsetattr(driver->getFD(), TCSANOW, &tty) != 0) {
+      std::cout << strerror(errno) << std::endl;
+  }
+
+  std::shared_ptr<NetworkBus> bus;
+  bus = std::make_shared<NetworkBus>(driver);
+
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<rclcpp::Node>("my_node");
+  auto publisher = node->create_publisher<std_msgs::msg::Int32>("dummy_pub", 10);
+  std::function<void(const std_msgs::msg::Int32::SharedPtr)> callback = std::bind(&dummy_callback, std::placeholders::_1, bus);
+  auto subscriber = node->create_subscription<std_msgs::msg::Int32>("dummy_sub", 10, callback);
+
+  bus->handle<DummySystem_DummyPacket>(&handle_dummy, (void*)&publisher);
+
+  std::cout << "SPIN" << std::endl;
+
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
+}
+
+
+
