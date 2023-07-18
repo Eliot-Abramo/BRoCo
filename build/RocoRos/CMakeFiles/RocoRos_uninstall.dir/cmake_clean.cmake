06f2ed4f17dfc7417c502eeaf0052b3207e8f54f
YassineB <ybakkali8@gmail.com>
2023-07-18 18:40:35 +0100

    Removed build files

--- build/RocoRos/CMakeFiles/RocoRos_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/RocoRos_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/RocoRos_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
