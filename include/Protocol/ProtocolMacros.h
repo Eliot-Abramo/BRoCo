3fcedff6eed24136361e75ad1d42e4aa42090f7c
Vincent Nguyen <vincen7.nguyen@gmail.com>
2023-09-01 20:01:10 +0200

    changed crc algorithm

--- include/Protocol/ProtocolMacros.h
+++ include/Protocol/ProtocolMacros.h
@@ -4,40 +4,16 @@
 
 #include <cstdint>
 
-#define CRC16 0x8005
-
-/* Begin copy-paste from https://gist.github.com/2666368 */
 static uint16_t __gen_crc16(const uint8_t *data, uint16_t size) {
-    uint16_t out = 0;
-    int bits_read = 0, bit_flag;
+    uint8_t x;
+    uint16_t crc = 0xFFFF;
 
-    /* Sanity check: */
-    if(data == nullptr)
-        return 0;
-
-    while(size > 0)
-    {
-        bit_flag = out >> 15;
-
-        /* Get next bit: */
-        out <<= 1;
-        out |= (*data >> (7 - bits_read)) & 1;
-
-        /* Increment bit counter: */
-        bits_read++;
-        if(bits_read > 7)
-        {
-            bits_read = 0;
-            data++;
-            size--;
-        }
-
-        /* Cycle check: */
-        if(bit_flag)
-            out ^= CRC16;
+    while (size--){
+        x = crc >> 8 ^ *data++;
+        x ^= x>>4;
+        crc = (crc << 8) ^ ((uint16_t)(x << 12)) ^ ((uint16_t)(x <<5)) ^ ((uint16_t)x);
     }
-
-    return out;
+    return crc;
 }
 
 #define STANDARD_PACKET(NAME, PACKET_DEF) struct NAME { PACKET_DEF } __attribute__((packed));
